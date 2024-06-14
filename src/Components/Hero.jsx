f77f994f0258768bfc2414df6c34ebabab3e2426
adamalcantara <alcantaradevelopment@gmail.com>
2024-06-13 22:41:59 -0400

    Hero section added and styled

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+
+import { heroVideo, smallHeroVideo } from '../utils';
 
 const Hero = () => {
+  const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo);
+
+  // Function to handle the video file based on the width of the display, using the state from above
+  const handleVideoSrcSet = () => {
+    if(window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo)
+    } else {
+      setVideoSrc(heroVideo)
+    }
+  }
+
+  // use effect to check for the resizing of the display, and then call the function which checks the display size
+  useEffect(() => {
+    window.addEventListener('resize', handleVideoSrcSet);
+
+    return () => {
+      window.removeEventListener('resize', handleVideoSrcSet)
+    }
+  }, [])
+
+  useGSAP(() => {
+    // GSAP to animation for the hero, set the opacity to 1 and delay for 1.5s
+    gsap.to('#hero', { opacity: 1, delay: 2 })
+
+    gsap.to('#cta', { opacity: 1, delay: 2, y: -50 })
+  }, [])
+
   return (
-    <div>Hero</div>
+    <section className='w-full nav-height bg-black relative'>
+      {/* Content container for hero */}
+      <div className='h-5/6 w-full flex-center flex-col'>
+        <p id='hero' className='hero-title'>iPhone 15 Pro</p>
+        <div className='md:w-10/12 w-9/12'>
+          <video className='pointer-events-none' autoPlay muted playsInline={true} key={videoSrc}>
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+
+      {/* call to action */}
+      <div id="cta" className='flex flex-col items-center opacity-0 translate-y-20'>
+        <a href="#highlights" className='btn'>Buy</a>
+        <p className='font-normal text-xl'>From $199/month or $999</p>
+      </div>
+    </section>
   )
 }
 
